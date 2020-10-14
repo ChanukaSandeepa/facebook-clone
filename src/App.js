92dfdc1097d8fd9e477e50dc778328166ca43c29
Chanuka <chanursandeepa@gmail.com>
2020-10-14 07:13:34 +0530

    Connection with firebase and setting up context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Feed from './components/Feed';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import './App.css'
 import Widgets from './components/Widgets';
+import Login from './components/Login';
+import AppContext, { useAppContext } from './config/AppContext';
 
 function App() {
 
+  const [login, setLogin] = useState(true)
+  // const [{ user } , dispatch] = useAppContext()
   useEffect(() => {
       document.title = "Facebook"
+      const name = "Chanuka sandeepa"
+      const rt = name.replace("s","f")
+      console.log(rt)
   },[])
+
+    // useEffect(() => {
+    //   if(user){
+    //     setLogin(true)
+    //   }
+    // },[user])
   
   return (
-    <div className="app">
-      <Header/>
-      <div className="app_body">
-        <Sidebar/>
-        <Feed/>
-        <Widgets/>
-      </div>
-    </div>
+    <AppContext>
+        <div className="app">
+      {!login ? <Login/> : (
+        <>
+        <Header/>
+          <div className="app_body">
+            <Sidebar/>
+            <Feed/>
+            <Widgets/>
+          </div>
+        </>
+      )}
+    </div> 
+    </AppContext>
+    
   );
 }
 
